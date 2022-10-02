1577ba85ababcc6db7e8ca42a7a26771ef6605e4
kshashwat007 <kshashwat007@gmail.com>
2022-10-02 13:28:59 +0530

    Premium folder in graph qs & graph valid tree qs

---
+++ Practice_questions/graphs/premium/graph_valid_tree.py
@@ -0,0 +1,18 @@
+# https://www.lintcode.com/problem/178/
+# Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of nodes), write a function to check whether these edges make up a valid tree.
+
+# Input: n = 5 edges = [[0, 1], [0, 2], [0, 3], [1, 4]]
+# Output: true.
+
+# Input: n = 5 edges = [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]]
+# Output: false.
+
+class Solution:
+    """
+    @param n: An integer
+    @param edges: a list of undirected edges
+    @return: true if it's a valid tree, or false
+    """
+
+    def valid_tree(self, n, edges):
+        pass
